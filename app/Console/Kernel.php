bf2e234bab6912ae9e5b476b72f018e2eac574d0
Wemerson Guimarães <wemersonrv@gmail.com>
2023-12-26 06:57:22 -0300

    Episode 7 - Laravel Task Scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +14,15 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+
+        // $schedule->call(function() {
+        //   Log::info('inline task: ' . now());
+        // })->everyMinute();
+
+        $schedule->command('upload:cleanup')
+                 ->everyMinute()
+//                 ->daily()
+                 ->emailOutputTo('wemersonrv@gmail.com');
     }
 
     /**
--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,19 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('upload:cleanup', function () {
+    $this->info('Cleaning up the temp uploads folder...');
+
+    $files = Storage::disk('public')->listContents('temp');
+
+    $deleted = collect($files)
+        ->filter(function($file) {
+            return $file['type'] === 'file' && $file['lastModified'] < now()->subDays(1)->getTimestamp();
+        })
+        ->each(function($file) use(&$c){
+            Storage::disk('public')->delete($file['path']);
+        })->count();
+
+    $this->info("$deleted file(s) have been deleted on " . now());
+})->purpose('Cleanup the temp uploads folder');
